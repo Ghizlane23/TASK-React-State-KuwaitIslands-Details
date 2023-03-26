600ba0f96046da597b9448df1022b3cb85426e65
ghizlane-essaidi <ghiza.essaidi@gmail.com>
2023-03-26 06:15:31 +0300

    Kuwait Island _spread operator to spread the input object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
+import { useState } from "react";
+import islands from "./data/islands";
 
 function App() {
-
-  let island = {
+  let isl = {
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  };
+  const [island, setIsland] = useState(isl);
+  const arr = islands.map((x) => 0);
+
+  const [count, setCount] = useState(arr);
 
   return (
-
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
-        <IslandForm island={island} />
+        <IslandList setIsland={setIsland} count={count} />
+        <IslandForm island={island} setCount={setCount} count={count} />
       </div>
     </div>
   );
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,46 @@
-export default function IslandForm({ island }) {
+import { useState } from "react";
+
+export default function IslandForm({ island, setCount, count }) {
+  const [input, setInput] = useState({ name: "", phone: "" });
+
+  function handleName(e) {
+    setInput({ ...input, name: e.target.value });
+  }
+  function handlePhone(e) {
+    setInput({ ...input, phone: e.target.value });
+  }
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input
+        onChange={handleName}
+        value={input.name}
+        type="text"
+        placeholder="Type Full Name"
+      />
+      <input
+        onChange={handlePhone}
+        value={input.phone}
+        type="number"
+        placeholder="Type Phone Number"
+      />
+      <button
+        className="book"
+        onClick={() => {
+          if (
+            window.confirm(
+              `Are you sure you want to book to ${island.name}  with the Name: ${input.name} and , phone: ${input.phone}`
+            )
+          ) {
+            const newCount = count.map((x) => x);
+            newCount[island.id - 1] = newCount[island.id - 1] + 1;
+            setCount(newCount);
+          }
+        }}
+      >
         Book for today!
       </button>
     </div>
